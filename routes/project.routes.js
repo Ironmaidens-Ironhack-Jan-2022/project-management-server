e518914c29e294c21c18d6b2a69e958fc5837922
Joanne Seiler <joanne@posteo.de>
2022-02-28 14:28:04 +0100

    change project routes

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const Project = require("../models/Project.model");
-
-// - POST /api/projects
-//   Project.create()
-//   req.body
-
+const mongoose = require("mongoose");
 
 router.post("/", (req, res) => {
 
@@ -15,10 +11,10 @@ router.post("/", (req, res) => {
   }
 
   Project.create(projectDetails)
-    .then( projectCreated => {
+    .then(projectCreated => {
       res.status(201).json(projectCreated)
     })
-    .catch( err => {
+    .catch(err => {
       console.log("error creating a new project", err);
       res.status(500).json({
         message: "error creating a new project",
@@ -27,5 +23,45 @@ router.post("/", (req, res) => {
     })
 
 })
+
+
+router.get('/', (req, res, next) => {
+  Project.find()
+    .populate('tasks')
+    .then(allProjects => res.json(allProjects))
+    .catch(err => res.json(err));
+});
+
+
+
+router.get('/:projectId', (req, res, next) => {
+  const { projectId } = req.params;
+ 
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Project.findById(projectId)
+    .populate('tasks')
+    .then(project => res.json(project))
+    .catch(err => res.status(500).json(err));
+});
+
+
+
+router.put('/:projectId', (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Project.findByIdAndUpdate(projectId, req.body, { new: true })
+    .then((updatedProject) => res.json(updatedProject))
+    .catch(error => res.json(error));
+});
+
 
 module.exports = router;
